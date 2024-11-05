42056b23a274fef6dd493252c84572c927c01220
Saravanan_19 <153452081+saravananCodeExplorer@users.noreply.github.com>
2024-11-05 12:10:56 +0530

    php ecommerce overall F&B connectivity project

---
+++ project/checkout.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>checkout</title>
+ <!-- css -->
+     <link rel="stylesheet" href="css/style.css">
+
+     <!-- jss -->
+      <script src="js/script.js"></script>
+
+
+       <!-- boostrap css -->
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+      <!-- font-awsewome -->
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  </head>
+  <body>
+               <!-- include footer -->
+      <?php include"header.php"?>
+      <!-- marquee -->
+<?php include"marquee.php"?>
+<!-- slider -->
+<?php include"slider.php"?>
+
+
+    <h1 style="text-align:center;">welcome to smartshooping!</h1>
+          <!--brand footer -->
+          <?php include"brand.php"?>
+             <!-- include footer -->
+      <?php include"footer.php"?> 
+  </body>
+</html>
